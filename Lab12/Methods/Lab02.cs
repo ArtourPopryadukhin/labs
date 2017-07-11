20422f83188ba57b2667912f8fea0c19e454ee57
akryukov <akryukov92@gmail.com>
2017-07-11 20:15:17 +0600

    Добавлены задания к 12 лабораторной

---
+++ Lab12/Methods/Lab02.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Lab02
+    {
+        public static double task4411(double a)
+        {
+            return a + 7;
+        }
+    } 
+}
